92e284d7eaeae9eff189d4e5c2284c9ad86841bd
rosera <askrichardrose@gmail.com>
2020-04-14 19:33:18 +0100

    Initial commit spl-data

---
+++ spl-data/index.js
@@ -0,0 +1,18 @@
+const data = require('./data/labs.json');
+const pug = require('pug');
+
+
+// Show page to be rendered
+function asyncRenderPage(req, res) {
+  const pugFile = pug.compileFile('views/index.pug');
+
+  console.log(`data: ${data.spls}`);
+  res.status(200).send(pugFile({
+	  labs: data.spls
+  }));
+}
+
+exports.labAPI=(req, res)=>{
+  console.log(`Hello from labAPI`);
+  asyncRenderPage(req, res);
+};
