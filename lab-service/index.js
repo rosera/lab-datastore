5e5f600671455add7c680685b46ce030e65180df
rosera <askrichardrose@gmail.com>
2020-04-25 12:58:00 +0100

    remove redundant files

--- lab-service/index.js
+++
@@ -1,9 +0,0 @@
-// Name: index.js
-// Description: Opens a port
-// 
-const app = require('./app.js');
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () =>
-  console.log(`Lab Service listening on port ${PORT}`)
-);
