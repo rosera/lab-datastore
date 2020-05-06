fa9f7b5592c055b144c2b67a4e5ec097ffe90a2a
rosera <askrichardrose@gmail.com>
2020-05-06 16:48:32 +0100

    change to express.js + update view templates

--- consumer/spl-data/index.js
+++ consumer/spl-data/index.js
@@ -1,44 +1,46 @@
+const express = require('express');
 const labdata = require('./data/details.json');
 const data = require('./data/labs.json');
+const path = require('path');
 const pug = require('pug');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 
+// Show the HOME page
 
-// Show page to be rendered
-function labRenderPage(req, res) {
-  const pugFile = pug.compileFile('views/lab.pug');
+app.get('',(req,res) => {
 
-  let pageRef = req.query.lab || '0';
-
-  res.status(200).send(pugFile({
-	  lab: labdata[0],
-	  name: pageRef
-  }));
-}
-
-
-
-// Show page to be rendered
-function asyncRenderPage(req, res) {
-  const pugFile = pug.compileFile('views/index.pug');
-
-  res.status(200).send(pugFile({
-	  labs: data
-  }));
-}
-
-exports.labAPI=(req, res)=>{
   let pageRef = req.query.lab || '0';
 
   console.log(pageRef);
 
   if (pageRef === "0") {
-    // Show the home page
-    console.log(`Hello from labAPI`);
-    asyncRenderPage(req, res);
+
+    res.render('index', {
+      labs: data 
+    });
   } else {
-    // Show the lab page
-    labRenderPage(req, res);
-    console.log("Calling lab");
+    res.render('lab', {
+      lab:labdata[0],
+      name: pageRef
+    });
   }
-};
+});
+
+
+// Show the Details page
+app.get('/', (req,res) => {
+  res.render('lab', {
+      lab:labdata[0],
+      name: pageRef
+  });
+});
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
